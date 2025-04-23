7486f6f5eddc87ad02ca0f389169375f7228773e
Carlos Orozco <carlosorozcok@gmail.com>
2025-04-22 23:34:53 -0600

    Cambios base de datos

---
+++ SistemaVentas/backend/TecnoSolucionesAPI/TecnoSolucionesAPI/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TecnoSolucionesAPI.Models
+{
+    public class Categoria
+    {
+
+        [Key]
+        public int IdCategoria { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string? Nombre { get; set; }
+
+        [MaxLength(256)]
+        public string? Descripcion {  get; set; }
+        
+        public bool Estado { get; set; }
+
+        public ICollection<Articulo>? Articulos { get; set; }
+    }
+}
